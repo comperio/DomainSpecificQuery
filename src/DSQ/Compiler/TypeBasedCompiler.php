bcdeaedcbf31e446988f892bedbb71ccb4b7fdc6
Nicolò Martini <electroportal.net@gmail.com>
2013-07-29 12:07:21 +0200

    TypeBased Compiler TypeBased Compiler first implementation. Two Compiler exceptions: InvalidTransformationException and UnregisteredTransformationException

---
+++ src/DSQ/Compiler/TypeBasedCompiler.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Compiler;
+
+
+use DSQ\Expression\Expression;
+
+class TypeBasedCompiler implements Compiler
+{
+    /**
+     * @var callable[]
+     */
+    private $transformations = array();
+
+    /**
+     * Register a transformation for the compiler
+     *
+     * @param string $type The type of the expression that will be transformed
+     * @param callable $transformation The transformation
+     * @return $this The current instance
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function registerTransformation($type, $transformation)
+    {
+        if (!is_callable($transformation))
+            throw new InvalidTransformationException('Transformations must be callable objects');
+
+        $this->transformations[$type] = $transformation;
+
+        return $this;
+    }
+
+    /**
+     * @param string $type
+     * @return bool
+     */
+    public function hasTransformation($type)
+    {
+        return isset($this->transformations[$type]);
+    }
+
+    /**
+     * Get the transformation for the given expression type
+     *
+     * @param string $type
+     *
+     * @return callable The transformation
+     *
+     * @throws UnregisteredTransformationException
+     */
+    public function getTransformation($type)
+    {
+        if (!$this->hasTransformation($type))
+            throw new UnregisteredTransformationException("There is no transformation registered for the Expression type \"$type\"");
+
+        return $this->transformations[$type];
+    }
+
+    /**
+     * @param Expression $expression
+     *
+     * @return mixed
+     */
+    public function compile(Expression $expression)
+    {
+        $transformation = $this->getTransformation($expression->getType());
+
+        return $transformation($expression, $this);
+    }
+}
