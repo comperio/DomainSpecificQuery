9b8c77a76684dc6e17ee3dc965cd004be80ef7e2
Nicolò Martini <electroportal.net@gmail.com>
2013-07-29 12:06:05 +0200

    The compiler interface

---
+++ src/DSQ/Compiler/Compiler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Compiler;
+
+use DSQ\Expression\Expression;
+
+interface Compiler
+{
+    /**
+     * @param Expression $expression
+     * @return mixed
+     */
+    public function compile(Expression $expression);
+} 
