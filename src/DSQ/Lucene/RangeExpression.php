ca3b26553b9886c4a3f16dfe4bd89a1177e38bc3
Nicolò Martini <electroportal.net@gmail.com>
2013-08-02 11:50:10 +0200

    RangeExpression implementation and tests

---
+++ src/DSQ/Lucene/RangeExpression.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Lucene;
+
+
+class RangeExpression extends BasicLuceneExpression
+{
+    private $includeLeft = true;
+    private $includeRight = true;
+
+    /**
+     * @param string $from
+     * @param string $to
+     * @param float $boost
+     * @param bool $includeLeft
+     * @param bool $includeRight
+     */
+    public function __construct($from = '*', $to = '*', $boost = 1.0, $includeLeft = true, $includeRight = true)
+    {
+        parent::__construct(array('from' => $from, 'to' => $to) , $boost, 'range');
+
+        $this->includeLeft = $includeLeft;
+        $this->includeRight = $includeRight;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        $value = $this->getValue();
+
+        $result = $this->includeLeft ? '[' : '{';
+        $result .= "{$this->escape($value['from'])} TO {$this->escape($value['to'])}";
+        $result .= $this->includeRight ? ']' : '}';
+        $result .= $this->boostSuffix();
+
+        return $result;
+    }
+
+    /**
+     * @param mixed $expression
+     * @return bool
+     */
+    public function hasPrecedence($expression)
+    {
+        return true;
+    }
+}
---
+++ tests/DSQ/Lucene/RangeExpressionTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Test\Lucene;
+
+
+use DSQ\Lucene\RangeExpression;
+
+class RangeExpressionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasPrecedence()
+    {
+        $expr = new RangeExpression;
+        $this->assertTrue($expr->hasPrecedence(null));
+    }
+
+    public function testToString()
+    {
+        $expr = new RangeExpression(10, 100);
+
+        $this->assertEquals('[10 TO 100]', (string) $expr);
+
+        $expr->setBoost(12.2);
+        $this->assertEquals('[10 TO 100]^12.2', (string) $expr);
+
+        $expr = new RangeExpression(':escapeme:', 100, 1.0, $includeLeft = true, $includeRight = false);
+        $this->assertEquals('[\:escapeme\: TO 100}', (string) $expr);
+    }
+}
+ 
