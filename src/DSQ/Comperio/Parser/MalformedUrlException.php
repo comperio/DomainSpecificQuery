735365f20c8d0aed202ef44fcddb97cee58cd30e
Nicolò Martini <electroportal.net@gmail.com>
2013-08-14 10:49:38 +0200

    Malformed Url exception class for UrlParsers.

---
+++ src/DSQ/Comperio/Parser/MalformedUrlException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Comperio\Parser;
+
+
+class MalformedUrlException extends \OutOfBoundsException
+{
+
+} 
