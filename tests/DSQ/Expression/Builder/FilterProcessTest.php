56c661a1caa7a0f65f7a5479549ef3b265e31f31
Nicolò Martini <electroportal.net@gmail.com>
2013-09-05 17:43:00 +0200

    Tests for filter process class.

---
+++ tests/DSQ/Expression/Builder/FilterProcessTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+namespace DSQ\Test\Expression\Builder;
+
+
+use Building\Context;
+use Building\DummyProcess;
+use DSQ\Expression\BasicExpression;
+use DSQ\Expression\Builder\FilterProcess;
+use DSQ\Expression\FieldExpression;
+use DSQ\Expression\TreeExpression;
+
+class FilterProcessTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuild()
+    {
+        $proc = new FilterProcess();
+        $context = new Context(null, '', new DummyProcess());
+
+        $newContext = $proc->build($context);
+        $expectedContext = new Context($context, null, $proc);
+
+        $this->assertEquals($expectedContext, $newContext);
+    }
+
+    public function testBuildWithoutGainingScope()
+    {
+        $proc = $this->getMock('DSQ\Expression\Builder\FilterProcess', array('subvalueBuilded'));
+        $context = new Context(null, new TreeExpression('and'), $proc);
+
+        $proc->expects($this->once())->method('subvalueBuilded');
+
+        $this->assertNull($proc->build($context, 'field', 'value'));
+    }
+
+    public function testSubvalueBuilded()
+    {
+        $proc = new FilterProcess('or');
+        $context = new Context(null, $tree = new TreeExpression('and'), $proc);
+
+        $proc->subvalueBuilded($context, $basic = new BasicExpression('foo'));
+        $children = $context->object->getChildren();
+        $subtree = $children[0];
+        $this->assertEquals('or', $subtree->getValue());
+        $this->assertEquals(array($basic), $subtree->getChildren());
+    }
+}
