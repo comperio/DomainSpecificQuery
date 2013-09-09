be996dfef4e6ff19e73acd66a105c1a24a72510a
Nicolò Martini <electroportal.net@gmail.com>
2013-09-09 15:58:38 +0200

    LabelCompiler example: convert an expression to a formatted string.

---
+++ examples/LabelCompiler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of DomainSpecificQuery
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicmartnic@gmail.com>
+ */
+
+include '../vendor/autoload.php';
+
+use DSQ\Expression\Builder\ExpressionBuilder;
+use DSQ\Compiler\Label\LabelCompiler;
+
+$builder = new ExpressionBuilder;
+$compiler = new LabelCompiler;
+
+$expr = $builder
+    ->field('title', 'foo')
+    ->field('date', '2120')
+    ->and()
+        ->field('name', 'john')
+        ->or()
+            ->field('lastname', 'smith')
+            ->field('lastname', 'rossi')
+        ->end()
+    ->end()
+    ->get()
+;
+
+echo '<pre>', $compiler->compile($expr);
